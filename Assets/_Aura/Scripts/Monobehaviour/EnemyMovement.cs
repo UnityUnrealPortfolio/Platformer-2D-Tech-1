5c38b1dd267353bf0150f02da6f001b2a866b79a
Auravision <auravision.africa@gmail.com>
2024-02-26 09:07:09 +0300

    Added minimal enemy waypoint behaviour

--- Assets/_Aura/Scripts/Monobehaviour/EnemyMovement.cs
+++ Assets/_Aura/Scripts/Monobehaviour/EnemyMovement.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Fields
+    [SerializeField] float moveSpeed;
+    bool isMovingRight = true; 
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+    #region Monobehaviour Callbacks
+    private void Update()
     {
-        
+        Vector2 moveDirection = SetDirection();
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("waypoint"))
+        {
+            FlipCharacter();
+        }
+    }
+    #endregion
+
+    #region Movement Utility
+    private Vector2 SetDirection()
+    {
+        Vector2 moveDirection = Vector2.zero;
+        if (isMovingRight)
+        {
+            moveDirection = new Vector2(1, 0);
+        }
+        else
+        {
+            moveDirection = new Vector2(-1, 0);
+        }
+
+        return moveDirection;
+    }
+    void FlipCharacter()
+    {
+        if (isMovingRight)
+        {
+            isMovingRight = false;
+            transform.localScale = new Vector3(-1, 1f, 1f);
+        }
+        else
+        {
+            isMovingRight = true;
+            transform.localScale = new Vector3(1, 1f, 1f);
+
+        }
+    } 
+    #endregion
 }
